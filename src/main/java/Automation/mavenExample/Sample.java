4da8e5e456b83eaf79c9fbd3bb720451d6dfa552
Navaneeth <navaneeth.info>
2020-01-22 15:07:35 +0530

    First commit to git

---
+++ src/main/java/Automation/mavenExample/Sample.java
@@ -0,0 +1,22 @@
+package Automation.mavenExample;
+
+import org.testng.annotations.Test;
+
+//
+//
+public class Sample {
+	//
+	@Test
+	public void test()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("maven testing");
+
+	}
+	@Test
+	public void test1()
+	{
+		System.out.println("test 1");
+	}
+
+}
